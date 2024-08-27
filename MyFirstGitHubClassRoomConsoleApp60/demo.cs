6e49c895281c62f0814ed88a753ad757105882f5
Christian Lund Nielsen <chn002@edu.zealand.dk>
2024-08-27 12:59:56 +0200

    Tilføjet properties

---
+++ MyFirstGitHubClassRoomConsoleApp60/demo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class demo
+    {
+       
+        //Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public demo()
+        {
+            
+        }
+
+        public demo(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
